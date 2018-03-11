096dcf7a32e9f55e48478d851d1a57fd1cad9177
jonschlinkert <jon.schlinkert@sellside.com>
2018-03-11 01:58:42 -0500

    first commit

---
+++ test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+require('mocha');
+var assert = require('assert');
+var yarnModules = require('./');
+
+describe('yarn-global-modules', function() {
+  it('should get the yarn global modules directory', function() {
+    assert.equal(typeof yarnModules(), 'string');
+    assert(/yarn/i.test(yarnModules()));
+  });
+});
