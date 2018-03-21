dc7e8a79117a4346472fb3679bb8c0d9323bae11
jonschlinkert <jon.schlinkert@sellside.com>
2018-03-21 14:54:52 -0400

    add tests, travis

--- test.js
+++ test.js
@@ -7,6 +7,7 @@ var yarnModules = require('./');
 describe('yarn-global-modules', function() {
   it('should get the yarn global modules directory', function() {
     assert.equal(typeof yarnModules(), 'string');
+    console.log(yarnModules())
     assert(/yarn/i.test(yarnModules()));
   });
 });
